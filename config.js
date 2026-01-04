4cfd5377c4dcdcb787cc860e722baaaa69af1617
DTZ X DULA <alphaxteam21@gmail.com>
2026-01-04 17:31:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "YPt2GRJC#7dCEokvK79hGQshanS8XpCOO97xNTVHjOCx4KR_4uG0",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
